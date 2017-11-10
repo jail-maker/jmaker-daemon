481f65d30e05b905f0acdd2ba7b7319e37141c41
Igor Bykov <igorb.dev@websm.io>
2017-11-10 14:38:04 +0300

    any chages

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 #!/usr/bin/env node
 
 const yaml = require('js-yaml');
+const minimist = require('minimist');
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const jailsDir = __dirname + '/jails';
+const port = 3346;
+const host = '0.0.0.0';
 const app = express();
 
 app.use(bodyParser.json());
 
-const port = 3346;
-const host = '0.0.0.0';
-
 app.post('/jails', (req, res) => {
 
-    console.log(req.body);
+    let configData = req.body;
+
+    tar.x({
+        file: configData.base + '.tar',
+        cwd: `${jailsDir}/${configData.name}`,
+    })
+        .then(_ => {
+            console.log('finish');
+        })
+
     res.send();
 
 });
