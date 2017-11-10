20bcb20fa4f6d219d24a7406cf0afe64bf800af4
Igor Bykov <igorb.dev@websm.io>
2017-11-10 16:44:20 +0300

    any changes

--- index.js
+++ index.js
@@ -1,33 +1,75 @@
 #!/usr/bin/env node
 
+const { spawn, spawnSync } = require('child_process')
 const yaml = require('js-yaml');
+const tar = require('tar');
+const fs = require('fs');
 const minimist = require('minimist');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const jailsDir = __dirname + '/jails';
 const port = 3346;
-const host = '0.0.0.0';
+const host = '127.0.0.1';
 const app = express();
 
 app.use(bodyParser.json());
 
 app.post('/jails', (req, res) => {
 
-    let configData = req.body;
+    console.log(req.body);
 
-    tar.x({
-        file: configData.base + '.tar',
-        cwd: `${jailsDir}/${configData.name}`,
-    })
-        .then(_ => {
-            console.log('finish');
-        })
+    let configData = req.body;
+    let jailDir = `${jailsDir}/${configData.name}`;
+
+    try {
+
+        fs.mkdirSync(jailDir);
+
+        tar.x({
+            file: configData.base + '.tar',
+            cwd: jailDir,
+            sync: true,
+        });
+
+    } catch(e) {
+
+        if (e.code !== 'EEXIST') {
+
+            console.log(e);
+            process.exit();
+
+        }
+
+    }
+
+    delete(configData.base);
+    delete(configData.rctl);
+    delete(configData.cpuset);
+
+    // let argv = jailPipe.start(configData)
+    //     .pipe(ip4)
+    //     .pipe(ip6)
+    //     .pipe(jailPipe.finish)
+
+    let result = spawnSync('jail', [
+        '-c', ...agv
+        `path=${jailDir}`,
+        `name=${configData.name}`,
+        `exec.start=${configData['exec.start']}`,
+    ]);
+
+    console.log(result.output[1].toString());
+    console.log(result.output[2].toString());
+
+    console.log('finish');
 
     res.send();
 
 });
 
 app.listen(port, host, () => {
+
     console.log(`listening on port ${port}!`);
+
 });
