170ab61b30a32b9cbef43fde98d44d59316d3602
Igor Bykov <igorb.dev@websm.io>
2017-12-21 17:08:29 +0300

    added recorder pool

---
+++ libs/recorder-pool.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const NotFoundError = require('./Errors/not-found-error.js');
+
+class RecorderPool {
+
+    constructor() {
+
+        this._pool = {};
+
+    }
+
+    set(name, recorder) {
+
+        this._pool[name] = recorder;
+
+    }
+
+    get(name) {
+
+        if(!this.has(name))
+            throw new NotFoundError(`Recorder ${name} not found.`);
+
+        return this._pool[name];
+
+    }
+
+    has(name) {
+
+        if(!this._pool[name])
+            return false;
+
+        return true;
+
+    }
+
+}
+
+module.exports = new RecorderPool;
