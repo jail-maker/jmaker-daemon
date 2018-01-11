4ae8a7d70fa90112827f3359a309eda44bc43e84
Bykov Igor <igorb.dev@websm.io>
2018-01-11 23:59:28 +0300

    new module j-prestart

---
+++ modules/j-prestart.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const { spawn, exec } = require('child_process');
+const logsPool = require('../libs/logs-pool.js');
+const ExecutionError = require('../libs/Errors/execution-error.js');
+
+class JPreStart {
+
+    constructor(jailName, commands = []) {
+
+        if (!Array.prototype.isPrototypeOf(commands))
+            commands = [commands];
+
+        this._jailName = jailName;
+        this._commands = commands;
+
+    }
+
+    async run() {
+
+        let commands = this._commands;
+        let log = logsPool.get(this._jailName);
+
+        for (let i = 0; i != commands.length; i++) {
+
+            layers.create(commands[i], storage => {
+
+                let command = `chroot ${storage.getPath()} ${commands[i]}`;
+                let child = exec(command, {
+                    stdio: ['ignore', 'pipe', 'pipe']
+                });
+
+                let { code } = await log.fromProcess(child);
+
+                if (code !== 0) {
+
+                    let msg = `Error execution command: ${command} .`;
+                    throw new ExecutionError(msg);
+
+                }
+
+            });
+
+        }
+
+    }
+
+    async rollback() { }
+
+}
+
+module.exports = JPreStart;
